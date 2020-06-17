7a7a2b534f74b6e257a5ef28c97575119b648044
Michał Rejdych <rejdych.m.r@gmail.com>
2020-06-17 19:13:43 +0200

    Updates

---
+++ precache-manifest.f4cdc21988042640ce553f0c2d58e551.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c38aeb5d85778e2ebb1fb1b0194a889a",
+    "url": "/Flip-Image-Game/index.html"
+  },
+  {
+    "revision": "3ca4d40f2a07554ed1cb",
+    "url": "/Flip-Image-Game/static/css/main.a2a55908.chunk.css"
+  },
+  {
+    "revision": "08ad948a0d830b8a6563",
+    "url": "/Flip-Image-Game/static/js/2.ca216007.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Flip-Image-Game/static/js/2.ca216007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ca4d40f2a07554ed1cb",
+    "url": "/Flip-Image-Game/static/js/main.dad0417d.chunk.js"
+  },
+  {
+    "revision": "f2a426d6ac269099015a",
+    "url": "/Flip-Image-Game/static/js/runtime-main.6a21897e.js"
+  }
+]);
